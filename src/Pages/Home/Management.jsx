12fb86f34ef749fb75a747937e011bce9626a3e0
Shimul <shimulmohammad420@gmail.com>
2023-12-25 04:57:27 +0600

    management secttion added

--- src/Pages/Home/Management.jsx
+++ src/Pages/Home/Management.jsx
@@ -1,54 +1,54 @@
-// import { GrProjects } from "react-icons/gr";
-// import { MdPeopleAlt } from "react-icons/md";
-// import { FaTasks } from "react-icons/fa";
+import { GrProjects } from "react-icons/gr";
+import { MdPeopleAlt } from "react-icons/md";
+import { FaTasks } from "react-icons/fa";
 
-// const Management = () => {
-//     return (
-//         <div className="container mx-auto">
-//             <div className="pb-16">
-//                 <div className="mb-12 pt-8">
-//                     <div className='flex border2 flex-col mx-auto text-center w-fit ' style={{
-//                         fontFamily: 'Inter'
-//                     }}>
-//                         <h2 className=' px-6 border-gray-300 py-5 text-3xl md:text-5xl font-extrabold text-[#484c7f] text-center'>&rdquo; Our Management System &rdquo;</h2>
-//                         <p className="text-gray-500 mb-3">Apps for anywhere</p>
-//                     </div >
-//                 </div>
-//                 <div className="container mx-auto">
-//                     <div className="grid grid-cols-1 gap-6 md:grid-cols-3 mb-14 mx-2">
-//                         <div className="flex bg-white p-10 border rounded-lg shadow-2xl flex-col items- justify-center animate__animated animate__fadeInDown ">
-//                             <div className="text-5xl text-[#484c7f]">
-//                                 <GrProjects></GrProjects>
-//                             </div>
-//                             <p className="mt-6 text- text-xl font-semibold text-[#484c7f]">
-//                                 Project Management
-//                             </p>
-//                             <p className="my-4 text-justify text-[#a3a3a6]">
-//                                 Project management is more than a to-do list. It means tracking tasks from beginning to end, delegating subtasks to teammates, and setting deadlines to make sure projects get done on time.</p>
-//                         </div>
-//                         <div className="flex bg-white p-10 rounded-lg shadow-2xl border flex-col justify-center animate__animated animate__delay-1s animate__fadeInDown">
-//                             <div className="text-5xl text-[#484c7f]">
-//                                 <MdPeopleAlt></MdPeopleAlt>
-//                             </div>
-//                             <p className="mt-6 text-xl font-semibold text-[#484c7f]">
-//                                 Hr Management
-//                             </p>
-//                             <p className="my-4 text-justify text-[#a3a3a6]">Hr management is the practice of managing people to achieve better performance.if you hire people into a business, you are looking for people who fit the company culture as they will be happier.</p>
-//                         </div>
-//                         <div className="flex bg-white rounded-lg shadow-2xl p-10 border flex-col justify-center animate__animated animate__delay-2s animate__fadeInDown">
-//                             <div className="text-5xl text-[#484c7f]">
-//                                 <FaTasks></FaTasks>
-//                             </div>
-//                             <p className="mt-6  text-xl font-semibold text-[#484c7f]">
-//                                 Task Board
-//                             </p>
-//                             <p className="my-4 text-justify text-[#a3a3a6]">The board allows to improve workflow visibility and ensures efficient diffusion of information relevant to the whole team. It helps team members to see their progress at a glance.</p>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// };
+const Management = () => {
+    return (
+        <div className="container mx-auto">
+            <div className="pb-16">
+                <div className="mb-12 pt-8">
+                    <div className='flex border2 flex-col mx-auto text-center w-fit ' style={{
+                        fontFamily: 'Inter'
+                    }}>
+                        <h2 className=' px-6 border-gray-300 py-5 text-3xl md:text-5xl font-extrabold text-[#484c7f] text-center'>&rdquo; Our Management System &rdquo;</h2>
+                        <p className="text-gray-500 mb-3">Apps for anywhere</p>
+                    </div >
+                </div>
+                <div className="container mx-auto">
+                    <div className="grid grid-cols-1 gap-6 md:grid-cols-3 mb-14 mx-2">
+                        <div className="flex bg-white p-10 border rounded-lg shadow-2xl flex-col items- justify-center animate__animated animate__fadeInDown ">
+                            <div className="text-5xl text-[#484c7f]">
+                                <GrProjects></GrProjects>
+                            </div>
+                            <p className="mt-6 text- text-xl font-semibold text-[#484c7f]">
+                                Project Management
+                            </p>
+                            <p className="my-4 text-justify text-[#a3a3a6]">
+                                Project management is more than a to-do list. It means tracking tasks from beginning to end, delegating subtasks to teammates, and setting deadlines to make sure projects get done on time.</p>
+                        </div>
+                        <div className="flex bg-white p-10 rounded-lg shadow-2xl border flex-col justify-center animate__animated animate__delay-1s animate__fadeInDown">
+                            <div className="text-5xl text-[#484c7f]">
+                                <MdPeopleAlt></MdPeopleAlt>
+                            </div>
+                            <p className="mt-6 text-xl font-semibold text-[#484c7f]">
+                                Hr Management
+                            </p>
+                            <p className="my-4 text-justify text-[#a3a3a6]">Hr management is the practice of managing people to achieve better performance.if you hire people into a business, you are looking for people who fit the company culture as they will be happier.</p>
+                        </div>
+                        <div className="flex bg-white rounded-lg shadow-2xl p-10 border flex-col justify-center animate__animated animate__delay-2s animate__fadeInDown">
+                            <div className="text-5xl text-[#484c7f]">
+                                <FaTasks></FaTasks>
+                            </div>
+                            <p className="mt-6  text-xl font-semibold text-[#484c7f]">
+                                Task Board
+                            </p>
+                            <p className="my-4 text-justify text-[#a3a3a6]">The board allows to improve workflow visibility and ensures efficient diffusion of information relevant to the whole team. It helps team members to see their progress at a glance.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
 
-// export default Management;
+export default Management;
