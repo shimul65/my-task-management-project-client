6fc8d2d255e2cc5462de9366baead220bb1a1090
Shimul <shimulmohammad420@gmail.com>
2023-12-25 04:56:46 +0600

    banner added with explore button

--- src/Pages/HelpCenter/HelpCenter.jsx
+++ src/Pages/HelpCenter/HelpCenter.jsx
@@ -1,17 +1,17 @@
 
-import Management from "../Home/Management";
-import WhoBenefit from "../Home/WhoBenefit";
-import HelpPageBanner from "./HelpPageBanner";
+// import Management from "../Home/Management";
+// import WhoBenefit from "../Home/WhoBenefit";
+// import HelpPageBanner from "./HelpPageBanner";
 
 
-const HelpCenter = () => {
-    return (
-        <div className="">
-            <HelpPageBanner></HelpPageBanner>
-            <Management></Management>
-            <WhoBenefit></WhoBenefit>
-        </div>
-    );
-};
+// const HelpCenter = () => {
+//     return (
+//         <div className="">
+//             <HelpPageBanner></HelpPageBanner>
+//             <Management></Management>
+//             <WhoBenefit></WhoBenefit>
+//         </div>
+//     );
+// };
 
-export default HelpCenter;
+// export default HelpCenter;
